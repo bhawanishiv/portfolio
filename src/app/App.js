c93724bbdb86b239f0a36e1420f5e70c3257ea09
Bhawani Shankar Bharti <bhawanishiv@gmail.com>
2022-09-11 00:42:54 +0530

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from 'app/components/Header';
+
+const App = () => {
+  const renderApp = () => <Header />;
+
+  return renderApp();
+};
+
+App.propTypes = {};
+
+App.defaultProps = {};
+
+export default App;
